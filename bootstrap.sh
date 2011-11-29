564dce2d6c21a27080e05a08b188ebd66e550a57
tgummerer <t.gummerer@gmail.com>
2011-11-29 22:56:54 +0100

    Made some changes, and added a bootstrap script (Thanks for the inspiration to Steve Losh), to make installing this configuration on another system easy

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Thanks to Steve Losh for the inspiration for this script. (https://bitbucket.org/sjl/shell-configuration/src)
+
+mkdir -p ~/lib/oh-my-zsh
+git clone git://github.com/tgummerer/oh-my-zsh ~/lib/oh-my-zsh
+
+# clean up old files
+rm ~/.zshrc
+rm -r ~/.vim
+rm -rf ~/.oh-my-zsh
+rm ~/.vimrc
+rm ~/.tmux.conf
+rm ~/.bashrc
+rm ~/.bash_profile
+rm ~/.gitconfig
+ln -s ~/lib/shell-configuration/.gitconfig ~/.gitconfig
+ln -s ~/lib/shell-configuration/.vim ~/.vim
+ln -s ~/lib/shell-configuration/.vimrc ~/.vimrc
+ln -s ~/lib/shell-configuration/.tmux.conf ~/.tmux.conf
+ln -s ~/lib/shell-configuration/.zshrc ~/.zshrc
