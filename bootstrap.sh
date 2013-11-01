71dac59ea02c729823a0d3a0dd312f3af6e1c357
Thomas Gummerer <t.gummerer@gmail.com>
2013-11-01 23:58:17 +0100

    more fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,25 +2,25 @@
 
 # Thanks to Steve Losh for the inspiration for this script. (https://bitbucket.org/sjl/shell-configuration/src)
 
-mkdir -p ~/lib/oh-my-zsh
-git clone git://github.com/tgummerer/oh-my-zsh ~/lib/oh-my-zsh
+mkdir -p $HOME/lib/oh-my-zsh
+git clone git://github.com/tgummerer/oh-my-zsh $HOME/lib/oh-my-zsh
 
 # clean up old files
-rm ~/.zshrc
-rm -r ~/.vim
-rm -rf ~/.oh-my-zsh
-rm ~/.vimrc
-rm ~/.tmux.conf
-rm ~/.bashrc
-rm ~/.bash_profile
-rm ~/.gitconfig
-rm ~/.muttrc
-rm -r ~/.mutt
-ln -s ~/lib/shell-configuration/.gitconfig ~/.gitconfig
-ln -s ~/lib/shell-configuration/.vim ~/.vim
-ln -s ~/lib/shell-configuration/.vimrc ~/.vimrc
-ln -s ~/lib/shell-configuration/.tmux.conf ~/.tmux.conf
-ln -s ~/lib/shell-configuration/.zshrc ~/.zshrc
-ln -s ~/lib/shell-configuration/.muttrc ~/.muttrc
-ln -s ~/lib/shell-configuration/.mutt ~/.mutt
-ln -s ~/lib/shell-configuration/.config ~/.config
+rm $HOME/.zshrc
+rm -r $HOME/.vim
+rm -rf $HOME/.oh-my-zsh
+rm $HOME/.vimrc
+rm $HOME/.tmux.conf
+rm $HOME/.bashrc
+rm $HOME/.bash_profile
+rm $HOME/.gitconfig
+rm $HOME/.muttrc
+rm -r $HOME/.mutt
+ln -s $HOME/lib/shell-configuration/.gitconfig $HOME/.gitconfig
+ln -s $HOME/lib/shell-configuration/.vim $HOME/.vim
+ln -s $HOME/lib/shell-configuration/.vimrc $HOME/.vimrc
+ln -s $HOME/lib/shell-configuration/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/lib/shell-configuration/.zshrc $HOME/.zshrc
+ln -s $HOME/lib/shell-configuration/.muttrc $HOME/.muttrc
+ln -s $HOME/lib/shell-configuration/.mutt $HOME/.mutt
+ln -s $HOME/lib/shell-configuration/.config $HOME/.config
